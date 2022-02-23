8d9e8a7acb5d8a8a5911ff94df22483a2f90d3d9
199-4 <199-41@mail.ru>
2022-02-23 14:31:37 +0400

    add_if_min "<" -> ">"

--- lab-client/src/main/java/com/ruskaof/client/commands/AddIfMinCommand.java
+++ lab-client/src/main/java/com/ruskaof/client/commands/AddIfMinCommand.java
@@ -23,7 +23,7 @@ public class AddIfMinCommand extends Command {
     public CommandResult execute(String arg) {
         StudyGroup studyGroup = new StudyGroupMaker(userInputManager, outputManager, collectionManager).makeStudyGroup();
 
-        if (collectionManager.getMainData().isEmpty() || studyGroup.compareTo(collectionManager.getMainData().first()) > 0) {
+        if (collectionManager.getMainData().isEmpty() || studyGroup.compareTo(collectionManager.getMainData().first()) < 0) {
             collectionManager.getMainData().add(studyGroup);
             return new CommandResult(false, "The element was added successfully");
         } else {
